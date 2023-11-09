6b736f9037d5f045fc1028b6adb3946b6ef4744b
Szymon Abramczyk <szymon.abramczykk@gmail.com>
2023-11-09 16:52:32 +0200

    Added input handling and basic movement for the player

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,16 +5,20 @@
 // Constructor that creates a Window object and creates the Player as a cyan recatngle 
 Game::Game() 
     : window_(sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" }), 
-    player_(sf::Vector2f(120, 50)) {
+    player_(sf::Vector2f(50, 90)),
+    player_speed_(500) {
     player_.setFillColor(sf::Color::Cyan);
+    player_.setPosition(200.f, 200.f);
     window_.setFramerateLimit(144);
 }
 
 // A method to run the game
 void Game::run() {
+    sf::Clock clock;
     while (window_.isOpen()) {
+        sf::Time delta_time = clock.restart();
         events();
-        update();
+        update(delta_time);
         render();
     }
 }
@@ -23,14 +27,33 @@ void Game::run() {
 void Game::events() {
     sf::Event event;
     while (window_.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-            window_.close();
+        switch (event.type) {
+            case sf::Event::Closed:
+                window_.close();
+                break;
+            case sf::Event::KeyPressed:
+                processPlayerInput(event.key.code, true); 
+                break;
+            case sf::Event::KeyReleased:
+                processPlayerInput(event.key.code, false); 
+                break;
         }
     }
 }
 
-void Game::update() {
-
+// A method to update, 
+// currently it just updates the position of the player on the screen
+void Game::update(sf::Time delta_time) {
+    sf::Vector2f movement(0.f, 0.f); 
+    if (is_moving_up_)
+        movement.y -= player_speed_; 
+    if (is_moving_down_)
+        movement.y += player_speed_; 
+    if (is_moving_left_)
+        movement.x -= player_speed_; 
+    if (is_moving_right_)
+        movement.x += player_speed_;
+    player_.move(movement * delta_time.asSeconds());
 }
 
 // A method to render objects to the creen
@@ -38,6 +61,18 @@ void Game::render() {
     window_.clear(); 
     window_.draw(player_); 
     window_.display();
+}
+
+// A method to change the information about certain keys being pressed 
+void Game::processPlayerInput(sf::Keyboard::Key key, bool is_pressed) {
+    if (key == sf::Keyboard::W) 
+        is_moving_up_ = is_pressed;
+    else if (key == sf::Keyboard::S) 
+        is_moving_down_ = is_pressed;
+    else if (key == sf::Keyboard::A) 
+        is_moving_left_ = is_pressed;
+    else if (key == sf::Keyboard::D) 
+        is_moving_right_ = is_pressed;
 }
 
 int main() {
--- src/Game.hpp
+++ src/Game.hpp
@@ -10,12 +10,18 @@ class Game {
 
     private:
         void events(); 
-        void update();
+        void update(sf::Time delta_time);
         void render();
+        void processPlayerInput(sf::Keyboard::Key key, bool isPressed);
 
     private:
         sf::RenderWindow window_; 
         sf::RectangleShape player_;
+        int player_speed_;
+        bool is_moving_up_ = false;
+        bool is_moving_down_ = false;
+        bool is_moving_left_ = false;
+        bool is_moving_right_ = false;
 };
 
 #endif
