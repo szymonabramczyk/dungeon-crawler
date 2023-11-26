f21606c91b17f0c61dc317888566493cb1da2116
Semenova Anastasiia <anastasiia.semenova@aalto.fi>
2023-11-26 14:52:37 +0200

    Add Item.hpp

---
+++ src/Item.hpp
@@ -0,0 +1,42 @@
+#ifndef ITEM_HPP_
+#define ITEM_HPP_
+
+#include <SFML/Graphics.hpp>
+
+class Item {
+public:
+    int amount;
+
+    Item(int amount);
+    virtual void useItem();
+    virtual void render(sf::RenderWindow& window, int x, int y) const;
+};
+
+class Weapon : public Item {
+public:
+    int damage;
+
+    Weapon(int amount, int damage);
+    void useItem() override;
+    void render(sf::RenderWindow& window, int x, int y) const override;
+};
+
+class Armour : public Item {
+public:
+    int defense;
+
+    Armour(int amount, int defense);
+    void useItem() override;
+    void render(sf::RenderWindow& window, int x, int y) const override;
+};
+
+class Potion : public Item {
+public:
+    int healingAmount;
+
+    Potion(int amount, int healingAmount);
+    void useItem() override;
+    void render(sf::RenderWindow& window, int x, int y) const override;
+};
+
+#endif
