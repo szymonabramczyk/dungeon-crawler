2e48bcdf4bae4f42cc9dd0209fd805185aae67c1
Szymon Abramczyk <szymon.abramczykk@gmail.com>
2023-12-04 12:37:49 +0200

    Automated loading of assets and improvements

--- src/Assets.cpp
+++ src/Assets.cpp
@@ -14,36 +14,52 @@ std::map<std::string, std::unique_ptr<sf::SoundBuffer>> Assets::soundBuffers;
 
 void Assets::loadAssets(const std::string& path) {
     std::string sprites_names[6] = {"player.png", "tiles.png", "orc.png", "health-potion.png", "orc-boss.png", "undead.png"};
+    std::string font_names[1] = {"Quinquefive-ALoRM.ttf"};
+    std::string sounds_names[5] = {"attack.wav", "collect.wav", "chest-open.wav", "level-up.wav", "game-over.wav"};
 
     std::filesystem::path cwd( std::filesystem::canonical( path ) );
     cwd = cwd.parent_path().parent_path();
 
-    // a method to automatically load sprites with given names into the sprite map
-    for (unsigned int i = 0; i < sprites_names->length(); i++) {
-        std::filesystem::path file = cwd / sprites_names[i];
-        std::string::size_type pos = sprites_names[i].find('.');
+    // a loop to automatically load sprites with given names into the sprite map
+    for (const auto& sprite_name : sprites_names) {
+        std::filesystem::path file = cwd / sprite_name;
+        std::string::size_type pos = sprite_name.find('.');
         if (pos != std::string::npos) {
-            std::cout << file << std::endl;
-            std::string tmp = sprites_names[i].substr(0, pos);
+            std::string tmp = sprite_name.substr(0, pos);
             loadSprite(tmp, file); 
         }
     }
-
-    std::filesystem::path file1 = cwd / "Quinquefive-ALoRM.ttf";
-    std::filesystem::path file2 = cwd / "attack.wav";
-    loadFont("Quinquefive-ALoRM", file1);
-    loadSound("attack", file2);
+    // a loop to automatically load fonts with given names into the sprite map
+    for (const auto& font_name : font_names) {
+        std::filesystem::path file = cwd / font_name;
+        std::string::size_type pos = font_name.find('.');
+        if (pos != std::string::npos) {
+            std::string tmp = font_name.substr(0, pos);
+            loadFont(tmp, file); 
+        }
+    }
+    // a loop to automatically load sounds with given names into the sprite map
+    for (const auto& sound_name : sounds_names) {
+        std::filesystem::path file = cwd / sound_name;
+        std::string::size_type pos = sound_name.find('.');
+        if (pos != std::string::npos) {
+            std::string tmp = sound_name.substr(0, pos);
+            loadSound(tmp, file); 
+        }
+    }
 } 
 
 // a method to load sprite with a given name and a path
 void Assets::loadSprite(const std::string& name, const std::string& filePath) {
-    sprites[name] = std::unique_ptr<SpriteInfo>(new SpriteInfo(filePath));
+    std::unique_ptr<SpriteInfo> sprite(new SpriteInfo(filePath));
+    sprites.insert(std::make_pair(name, std::move(sprite)));
 }
 
 // a method to load font with a given name and a path
 void Assets::loadFont(const std::string& name, const std::string& filePath) {
     std::unique_ptr<sf::Font> font(new sf::Font());
-    font->loadFromFile(filePath);
+    if (!font->loadFromFile(filePath))
+        throw std::runtime_error("Couldn't load " + name + " font file: " + filePath);
     fonts.insert(std::make_pair(name, std::move(font)));
 }
 
@@ -51,7 +67,7 @@ void Assets::loadFont(const std::string& name, const std::string& filePath) {
 void Assets::loadSound(const std::string& name, const std::string& filePath) {
     std::unique_ptr<sf::SoundBuffer> buffer(new sf::SoundBuffer());
     if (!buffer->loadFromFile(filePath))
-        throw std::runtime_error("Couldn't load sound file!");
+        throw std::runtime_error("Couldn't load " + name + " sound file: " + filePath);
 
     std::unique_ptr<sf::Sound> sound(new sf::Sound()); 
     sound->setBuffer(*buffer);
