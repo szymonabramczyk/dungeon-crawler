4b8bba58b34915ddbe96afbfaecdd7aa618be60b
Szymon Abramczyk <szymon.abramczykk@gmail.com>
2023-12-04 12:37:10 +0200

    Added throwing an exception

--- src/SpriteInfo.hpp
+++ src/SpriteInfo.hpp
@@ -13,7 +13,7 @@ struct SpriteInfo // structers that stores assets info - texture size and hitbox
         mFramesPerRow(framesPerRow)
     {   
         if (!mTexture.loadFromFile(path))
-            std::cout << "Texture " << path << "couldn't be loaded" << std::endl;
+            throw std::runtime_error("Couldn't load sprite file: " + path);
         mFrameDim = sf::Vector2f(mTexture.getSize().x/mFramesPerRow, mTexture.getSize().y/(mFrames/mFramesPerRow));
         mHitBox = sf::FloatRect(0.f, 0.f, mFrameDim.x, mFrameDim.y);
     }
